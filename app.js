28b672fdb27f9ee6cc84674dfbc8e3ecd7cef22a
Astha Sharma <a@astha.me>
2016-04-27 19:07:29 +0530

    Displayed temperature in celsius from weather Api

--- app.js
+++ app.js
@@ -10,14 +10,50 @@ function getLocation(){
         })
     }
 }
+
 function getWeather(){
     var xhr = new XMLHttpRequest();
-    var url = "http://api.openweathermap.org/data/2.5/forecast/weather?lat=" + lat + "&lon=" + long + "&APPID=173d45a8a4cea9f316f3142c74393309";
-    xhr.open("GET", url, false);
+    var url = "http://api.openweathermap.org/data/2.5/forecast/weather?lat=" + lat + "&lon=" + long + "&units=metric&APPID=173d45a8a4cea9f316f3142c74393309";
+
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState == 4 & xhr.status == 200){
+            var data = JSON.parse(xhr.responseText);
+            console.log(data);
+            display(data);
+        }
+    }
+    xhr.open("GET", url, true);
     xhr.send();
 
     console.log(xhr.status);
     console.log(xhr.statusText);
 }
 
+function display(data){
+    displayImg(data);
+    var output = '';
+    output += '<h2>' + data.city.name + '</h2>';
+    output += '<h4>' + data.list[0].weather[0].main + '</h4>';
+    output += '<h5>' + data.list[0].weather[0].description + '</h5>';
+    output += '<h1>' + data.list[0].main.temp + '\xB0C</h1>';
+    document.getElementById("data").innerHTML = output;
+}
+function displayImg(data){
+        var main = data.list[0].weather[0].main;
+        var description = data.list[0].weather[0].description;
+        console.log(data);
+        if(main.indexOf("Cloud") > -1 && description.indexOf("Overcast") > -1){
+            document.getElementById("back-img").style.backgroundImage="url(images/overcast.jpg)";
+        } else if(main.indexOf("Wind") > -1){
+            document.getElementById("back-img").style.backgroundImage="url(images/windy.jpg)";
+        } else if(main.indexOf("Sun") > -1){
+            document.getElementById("back-img").style.backgroundImage="url(images/sunny.jpg)";
+        } else if(main.indexOf("Rain") > -1){
+            document.getElementById("back-img").style.backgroundImage="url(images/rainy.jpg)";
+        }else if(main.indexOf("Cloud") > -1){
+            document.getElementById("back-img").style.backgroundImage="url(images/cloudy.jpg)";
+        }else{
+            document.getElementById("back-img").style.backgroundImage="url(images/snow.jpg)";
+        }
+}
 getLocation();
